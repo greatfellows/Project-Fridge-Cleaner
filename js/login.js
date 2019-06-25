d648d797a422504a1c9c70afdf6ad6029252de2a
elle-young <missanyayoung@outlook.com>
2019-06-25 13:58:01 -0700

    log in functionality, send user input to LS

--- js/login.js
+++ js/login.js
@@ -1,13 +1,15 @@
 'use strict';
 
-
-
 var loginButton = document.getElementById('submit');
 loginButton.addEventListener('click', submitLogin);
 function submitLogin(){
-  event.preventDefault();
-  //add username and password to object User/local storage;
-  localStorage.setItem('username', 'password');
-  console.log(username.value);
+
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+
+  console.log(username);
+  console.log(password);
+
+  handleLogin(username, password);
 }
-//expected string arguments username and password
