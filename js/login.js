d88bfbddd1b84c50840bc5d5aaef841a0b5fbc8d
Benjamin Clark <benisin@gmail.com>
2019-06-26 09:03:05 -0700

    will check password if user already exists. alerts if password is wrong.

--- js/login.js
+++ js/login.js
@@ -1,32 +1,47 @@
 'use strict';
 
-
+// add event listener to login button - triggers popup login form
 var loginButton = document.getElementById('loginbutton');
 loginButton.addEventListener('click', popupForm);
-function popupForm(event){
-  console.log(event);
+
+// change id of login form to fromVis to style visible
+function popupForm() {
   var popupForm = document.getElementById('formInvis');
-
   popupForm.id = 'formVis';
-
 }
 
-
+// add evnet listener to login form submit button - triggers submitLogin call
 var submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', submitLogin);
-function submitLogin(){
+
+// get username and password from login form and pass as arguments to function handleLogin() on app.js
+function submitLogin() {
 
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
 
+  //check if password entered matches password stored
+  // if user already exists
+  if (allUsersData[username]) {
 
-  console.log(username);
-  console.log(password);
+    // if user password matches - call login
+    if (password === allUsersData[username].password) {
+      handleLogin(username, password);
 
-  handleLogin(username, password);
+      //if user password doesn't match alert message
+    } else {
+      alert('Sorry, the password you entered does not match password for user ' + username);
+    }
+
+    // if user doesn't exist - call login
+  } else {
+    handleLogin(username, password);
+  }
+
+
+  // make login form invisible again
   var popDown = document.getElementById('formVis');
   popDown.id = 'formInvis';
- 
 }
 
 
